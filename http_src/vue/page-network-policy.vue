5d24cdc4e3bf2f8b5c93d2479c9b2871c73c5096
Matteo Biscosi <biscosi@ntop.org>
2025-03-05 16:46:41 +0100

    Added import/export in network policy page

--- http_src/vue/page-network-policy.vue
+++ http_src/vue/page-network-policy.vue
@@ -7,7 +7,8 @@
         <div class="card card-shadow" :class="[(!props.context.is_check_enabled) ? 'ntopng-gray-out' : '']">
             <Loading v-if="show_spinner"></Loading>
             <div class="card-body">
-                <table class="table table-striped table-bordered col-sm-12" :class="show_spinner ? 'ntopng-gray-out' : ''">
+                <table class="table table-striped table-bordered col-sm-12"
+                    :class="show_spinner ? 'ntopng-gray-out' : ''">
                     <tbody class="table_length">
                         <tr v-for="(value, key) in check_name" :key="key" class="mb-4">
                             <td>
@@ -29,11 +30,21 @@
                         </tr>
                     </tbody>
                 </table>
+                <div v-if="show_configuration_error" class="text-danger">
+                    <span v-html="configuration_error"></span>
+                </div>
                 <div class="d-flex justify-content-between">
-                    <div>
-                        <div v-if="show_configuration_error" class="text-danger">
-                            <span v-html="configuration_error"></span>
-                        </div>
+                    <div clas="align-items-center ms-auto">
+                        <button class="btn btn-primary me-2" type="button" @click="import_policies">
+                            <i class="fa-solid fa-file-arrow-up" data-bs-toggle="tooltip" data-bs-placement="top"
+                                :title="_i18n('network_configuration.import_policies')"></i> {{
+                                    _i18n('network_configuration.import_policies') }}
+                        </button>
+                        <a class="btn btn-primary" download="network_policies.json" :href="export_network_policies_url">
+                            <i class="fa-solid fa-file-arrow-down" data-bs-toggle="tooltip" data-bs-placement="top"
+                                :title="_i18n('network_configuration.export_policies')"></i> {{
+                                    _i18n('network_configuration.export_policies') }}
+                        </a>
                     </div>
                     <div clas="align-items-center ms-auto">
                         <button class="btn btn-primary" :disabled="disable_save" @click="reloadNetworks">
@@ -45,6 +56,10 @@
         </div>
         <NoteList :note_list="notes"> </NoteList>
     </div>
+
+    <ModalImportNetworkPolicies ref="modal_import_network_policies" :context="context"
+        @add="import_network_policies_rest">
+    </ModalImportNetworkPolicies>
 </template>
 
 <script setup>
@@ -52,6 +67,7 @@ import { ref, reactive, onMounted, computed } from 'vue'
 import { ntopng_utility } from "../services/context/ntopng_globals_services.js";
 import { default as NoteList } from "./note-list.vue";
 import { default as Loading } from "./loading.vue";
+import { default as ModalImportNetworkPolicies } from "./modal-import-network-policies.vue"
 import regexValidation from "../utilities/regex-validation.js";
 
 const _i18n = (t) => i18n(t);
@@ -69,7 +85,10 @@ const networks = reactive({});
 const validationErrors = reactive({});
 const set_config_url = `${http_prefix}/lua/pro/rest/v2/set/network/policy.lua`
 const get_config_url = `${http_prefix}/lua/pro/rest/v2/get/network/policy.lua`
+const import_network_policies_url = `${http_prefix}/lua/pro/rest/v2/add/network/policy.lua`;
+const export_network_policies_url = `${http_prefix}/lua/pro/rest/v2/export/network/policy.lua`;
 const modifiedInputs = ref([]);
+const modal_import_network_policies = ref();
 
 const error_message = ref(_i18n('network_configuration.policy_note'))
 const configuration_error = ref('');
@@ -109,6 +128,30 @@ Object.keys(check_name).forEach(key => {
 onMounted(() => {
     getConfig();
 });
+
+/* ************************************** */
+
+function import_policies() {
+    modal_import_network_policies.value.show();
+}
+
+const import_network_policies_rest = async function (params) {
+    const url = import_network_policies_url;
+    const result = await ntopng_utility.http_post_request(url, { ...{ csrf: props.context.csrf }, ...params }, false, true);
+
+    if (result == null) {
+        modal_import_network_policies.value.show_bad_feedback(_i18n("import_network_policies_error"));
+    } else if (result.rc < 0) {
+        modal_import_network_policies.value.show_bad_feedback(result.rsp.feedback);
+        table_network_policies.value.refresh_table();
+    } else {
+        setTimeout(() => { modal_import_network_policies.value.close() }, 1000);
+
+        getConfig();
+    }
+}
+
+/* ************************************** */
 
 // Function used to populate text area with data received from the backend at page initialization
 const getConfig = async () => {
@@ -153,8 +196,8 @@ const validateNetworkAddresses = () => {
                 if (regexValidation.validateMAC(net)) {
                     fixed_networks.push(net);
                     return;
-                }    
-            } 
+                }
+            }
             validationErrors[key] = 'Invalid Network format';
             isValid = false;
         })
