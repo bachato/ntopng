7120b4742bddaa41f02f0cd9a6061ff7aa9ca989
Luca Deri <deri@ntop.org>
2025-03-31 08:52:22 +0200

    Added fix for non-packt based analysis

--- src/Flow.cpp
+++ src/Flow.cpp
@@ -8834,7 +8834,9 @@ void Flow::updateTCPHostServices(Host *cli_h, Host *srv_h) {
   switch (ndpi_get_lower_proto(ndpiDetectedProtocol)) {
   case NDPI_PROTOCOL_MAIL_SMTPS:
   case NDPI_PROTOCOL_MAIL_SMTP:
-    if(isBidirectional() && isThreeWayHandshakeOK() && (getConfidence() == NDPI_CONFIDENCE_DPI)) {
+    if(isBidirectional() && isThreeWayHandshakeOK()
+       // && (getConfidence() == NDPI_CONFIDENCE_DPI)
+       ) {
       if(srv_h)
 	srv_h->setSmtpServer(domain_name);
       else if(srv_ip_addr)
@@ -8844,7 +8846,9 @@ void Flow::updateTCPHostServices(Host *cli_h, Host *srv_h) {
 
   case NDPI_PROTOCOL_MAIL_IMAPS:
   case NDPI_PROTOCOL_MAIL_IMAP:
-    if(isBidirectional() && isThreeWayHandshakeOK() && (getConfidence() == NDPI_CONFIDENCE_DPI)) {
+    if(isBidirectional() && isThreeWayHandshakeOK()
+       // && (getConfidence() == NDPI_CONFIDENCE_DPI)
+       ) {
       if(srv_h)
 	srv_h->setImapServer(domain_name);
       else if(srv_ip_addr)
@@ -8854,7 +8858,9 @@ void Flow::updateTCPHostServices(Host *cli_h, Host *srv_h) {
 
   case NDPI_PROTOCOL_MAIL_POPS:
   case NDPI_PROTOCOL_MAIL_POP:
-    if(isBidirectional() && isThreeWayHandshakeOK() && (getConfidence() == NDPI_CONFIDENCE_DPI)) {
+    if(isBidirectional() && isThreeWayHandshakeOK()
+       // && (getConfidence() == NDPI_CONFIDENCE_DPI)
+       ) {
       if(srv_h)
 	srv_h->setPopServer(domain_name);
       else if(srv_ip_addr)
