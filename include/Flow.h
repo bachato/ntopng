511e3ed9e2de1c8bce40de7d76ecd5eb5b2a0973
Alfredo Cardigliano <cardigliano@ntop.org>
2025-04-03 17:30:09 +0200

    Use MOS in QoE computation

--- include/Flow.h
+++ include/Flow.h
@@ -697,6 +697,7 @@ public:
 		       u_int8_t *srv_to_cli_qoe, std::vector<std::string> *srv_to_cli_qoe_issues);
   u_int8_t computeQoETCPscore(QoELimits *l, bool cli_to_srv, std::vector<std::string> *issues);
   u_int8_t computeQoEUDPscore(QoELimits *l, bool cli_to_srv, std::vector<std::string> *issues);
+  u_int8_t computeQoEMOSscore(bool cli_to_srv);
   u_int8_t getQoEScore();
   void getQoEInfo(ndpi_serializer *serializer);
   QoEType getQoEType();
