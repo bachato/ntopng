5dcafe03f9beae957b9fd4f641a6cd7d4a43ea9b
Alfredo Cardigliano <cardigliano@ntop.org>
2025-07-14 14:50:32 +0200

    Use small breakpoint (sm) cards in dashboard to be responsive

--- http_src/vue/dashboard-box.vue
+++ http_src/vue/dashboard-box.vue
@@ -29,7 +29,7 @@ const props = defineProps({
 });
 
 const width_class = computed(() => {
-  return `col-${props.width || 4}`;
+  return `col-sm-${props.width || 4}`;
 });
 
 const height_class = computed(() => {
