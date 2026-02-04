a635a036fbd0e833d3195886c3cffdbe4aa5d5b6
Alfredo Cardigliano <cardigliano@ntop.org>
2026-02-04 15:44:00 +0100

    Fix export string

--- scripts/locales/en.lua
+++ scripts/locales/en.lua
@@ -6256,7 +6256,7 @@ local lang = {
     ["after_reset_request"] = "<b>Restart</b> %{product} to make the configuration factory reset effective.",
     ["checks"] = "Behavioural Checks",
     ["config_import"] = "Import <span class='item'>{{ type }}</span> Configuration",
-    ["everything"] = "Entire %{product} configuration (includes users, preferences, and all configurations below). It is recommended to run this action on a clean intallation only (or after a Factory Reset).",
+    ["everything"] = "Entire %{product} configuration (includes users, preferences, and all configurations below). It is recommended to import the configuration on a clean intallation only (or after a Factory Reset).",
     ["export_policy"] = "Export nProbe IPS Policy",
     ["infrastructure_instances"] = "Infrastructure Instances",
     ["manage_configuration"] = "Manage Configurations",
