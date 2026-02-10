04448863f6f956c6d80c6114e00123ac9d618bb2
Luca Deri <deri@ntop.org>
2026-02-10 21:46:55 +0100

    Indent

--- src/LuaEngine.cpp
+++ src/LuaEngine.cpp
@@ -1324,9 +1324,8 @@ int LuaEngine::handle_script_request(struct mg_connection *conn,
   lua_newtable(L);
   lua_push_str_table_entry(L, "REQUEST_METHOD",
                            (char *)request_info->request_method);
-  lua_push_str_table_entry(
-      L, "URI",
-      (char *)request_info->uri ? (char *)request_info->uri : (char *)"");
+  lua_push_str_table_entry(L, "URI",
+			   (char *)request_info->uri ? (char *)request_info->uri : (char *)"");
   lua_push_str_table_entry(L, "REFERER",
                            (char *)mg_get_header(conn, "Referer")
 			   ? (char *)mg_get_header(conn, "Referer")
