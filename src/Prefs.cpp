1c56aeaa132ce577e07b100a0f4e15b78a13de41
Luca <deri@ntop.org>
2026-06-29 17:56:51 +0200

    Typo

--- src/Prefs.cpp
+++ src/Prefs.cpp
@@ -3661,13 +3661,13 @@ void Prefs::setS7CommAllowedFunctionCodes(const char* function_codes) {
 /* *************************************** */
 
 void Prefs::reloadServersConfiguration() {
-ntop->getTrace()->traceEvent(TRACE_NORMAL, "RELOADING servers configurations");
+ntop->getTrace()->traceEvent(TRACE_NORMAL, "Reloading servers configurations");
   gateway->reloadServerConfiguration((char*)CONST_GATEWAY_CONFIGURATION_REDIS_KEY);
   dns_servers->reloadServerConfiguration((char*)CONST_DNS_SERVER_CONFIGURATION_REDIS_KEY);
   ntp_servers->reloadServerConfiguration((char*)CONST_NTP_SERVER_CONFIGURATION_REDIS_KEY);
-ntop->getTrace()->traceEvent(TRACE_NORMAL, "RELOADING DHCP servers...");
+ntop->getTrace()->traceEvent(TRACE_NORMAL, "Reloading DHCP servers...");
   dhcp_servers->reloadServerConfiguration((char*)CONST_DHCP_SERVER_CONFIGURATION_REDIS_KEY);
-ntop->getTrace()->traceEvent(TRACE_NORMAL, "RELOADING DHCP servers done");
+ntop->getTrace()->traceEvent(TRACE_NORMAL, "Reloading DHCP servers done");
   smtp_servers->reloadServerConfiguration((char*)CONST_SMTP_SERVER_CONFIGURATION_REDIS_KEY);
 }
 
